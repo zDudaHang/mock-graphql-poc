3fc76163ff9143843cadfc0bed05a0f21982b5a1
zDudaHang <maria.eduarda.melo.hang@gmail.com>
2021-10-27 12:48:12 -0300

    Adicionando a lib

--- cypress/integration/init.spec.ts
+++ cypress/integration/init.spec.ts
@@ -1,4 +1,12 @@
 describe("Cypress", () => {
+  beforeEach(() => {
+    cy.readFile("./src/graphql/introspection.generated.json").then((schema) => {
+      cy.mockNetwork({ schema });
+    });
+    
+    cy.visit("http://localhost:3000");
+  });
+
   it("is working", () => {
     expect(true).to.equal(true);
   });
